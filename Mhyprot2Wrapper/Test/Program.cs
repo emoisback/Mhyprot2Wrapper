df50236b9636532a6321599f8a967aa4f8fc4941
Nagataka <irsyadpso2@gmail.com>
2022-07-12 08:39:56 +0700

    Add files via upload

---
+++ Mhyprot2Wrapper/Test/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+using Mhyprot2Wrapper;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Library library = new Library();
+            library.OpenProcess((uint)Process.GetProcessesByName("ProjectN-Win64-Shipping")[0].Id);
+            //Write to memory
+            library.Write<float>(700, "ProjectN-Win64-Shipping.exe+0636F5C8,0,A0,288,18C");
+            //Read from memory
+            var x = library.Read<float>("ProjectN-Win64-Shipping.exe+0636F5C8,0,A0,288,18C");
+            Console.WriteLine("Movespeed : "+x);
+            library.CloseDriver();
+            Console.ReadLine();
+        }
+    }
+}
